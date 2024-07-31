d2bdfa6fb854118cc9787471eb9076fbf46770f2
Kainy <iartkhay@gmail.com>
2024-07-31 02:45:23 +0100

    fix side nav logout to documentation and signeed in user name

--- src/components/global/Sidebar.tsx
+++ src/components/global/Sidebar.tsx
@@ -1,27 +1,60 @@
-import api from '@/config/axios'
-import { navLinks } from '@/utils/data'
-import logo from '@images/fumiwo-logo.png'
-import { Link, NavLink, useNavigate } from 'react-router-dom'
-
-
+import { navLinks } from "@/utils/data";
+import logo from "@images/fumiwo-logo.png";
+import { Link, NavLink } from "react-router-dom";
 
 const bottomNavLinks = [
-	{
-		name: 'Logout',
-		Icon: <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path d="M15.2409 22.2705H15.1109C10.6709 22.2705 8.53094 20.5205 8.16094 16.6005C8.12094 16.1905 8.42094 15.8205 8.84094 15.7805C9.24094 15.7405 9.62094 16.0505 9.66094 16.4605C9.95094 19.6005 11.4309 20.7705 15.1209 20.7705H15.2509C19.3209 20.7705 20.7609 19.3305 20.7609 15.2605V8.74047C20.7609 4.67047 19.3209 3.23047 15.2509 3.23047H15.1209C11.4109 3.23047 9.93094 4.42047 9.66094 7.62047C9.61094 8.03047 9.26094 8.34047 8.84094 8.30047C8.74238 8.29277 8.64632 8.26569 8.55825 8.22077C8.47018 8.17586 8.39185 8.114 8.32774 8.03875C8.26364 7.96349 8.21502 7.87632 8.18468 7.78224C8.15434 7.68815 8.14288 7.589 8.15094 7.49047C8.49094 3.51047 10.6409 1.73047 15.1109 1.73047H15.2409C20.1509 1.73047 22.2509 3.83047 22.2509 8.74047V15.2605C22.2509 20.1705 20.1509 22.2705 15.2409 22.2705Z" fill="#FF0000" />
-			<path d="M14.9972 12.75H3.61719C3.20719 12.75 2.86719 12.41 2.86719 12C2.86719 11.59 3.20719 11.25 3.61719 11.25H14.9972C15.4072 11.25 15.7472 11.59 15.7472 12C15.7472 12.41 15.4072 12.75 14.9972 12.75Z" fill="#FF0000" />
-			<path d="M5.8477 16.0998C5.6577 16.0998 5.4677 16.0298 5.3177 15.8798L1.9677 12.5298C1.82822 12.3887 1.75 12.1983 1.75 11.9998C1.75 11.8014 1.82822 11.611 1.9677 11.4698L5.3177 8.11984C5.6077 7.82984 6.0877 7.82984 6.3777 8.11984C6.6677 8.40984 6.6677 8.88984 6.3777 9.17984L3.5577 11.9998L6.3777 14.8198C6.6677 15.1098 6.6677 15.5898 6.3777 15.8798C6.2377 16.0298 6.0377 16.0998 5.8477 16.0998Z" fill="#FF0000" />
-		</svg>,
-		link: '',
-	}
-]
+  {
+    name: "Documentation",
+    Icon: (
+      <svg
+        width="24"
+        height="25"
+        viewBox="0 0 24 25"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M11 22.5H16C19.5 22.5 21 20.5 21 17.5V7.5C21 4.5 19.5 2.5 16 2.5H8C4.5 2.5 3 4.5 3 7.5V14.5"
+          stroke="#A5B3CD"
+          stroke-width="1.5"
+          stroke-miterlimit="10"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M14.5 5V7C14.5 8.1 15.4 9 16.5 9H18.5"
+          stroke="#A5B3CD"
+          stroke-width="1.5"
+          stroke-miterlimit="10"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M4 17.5L2 19.5L4 21.5"
+          stroke="#A5B3CD"
+          stroke-width="1.5"
+          stroke-miterlimit="10"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M7 17.5L9 19.5L7 21.5"
+          stroke="#A5B3CD"
+          stroke-width="1.5"
+          stroke-miterlimit="10"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    ),
+    link: "",
+  },
+];
 
 const Sidebar = () => {
-	const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-
-	return (
+  return (
     <div className="hidden h-screen min-w-[276px] flex-col border-r border-sidebarBorder bg-white lg:flex">
       <section className="flex h-20 items-center border-b border-otpBox pl-12">
         <Link to="/dashboard/overview">
@@ -57,20 +90,7 @@ const Sidebar = () => {
           {bottomNavLinks.map((link, index) => (
             <button
               key={index}
-              className={`flex items-center gap-4 px-4 py-3 text-lg font-medium text-absoluteRed duration-300`}
-              // onClick={() => {
-              // 	console.log('logging out...');
-              // 	localStorage.clear();
-              // 	api.defaults.headers.common['Authorization'] = undefined
-              // 	redirect('/login')
-              // }}
-              onClick={() => {
-                console.log("logging out...");
-
-                localStorage.clear();
-                api.defaults.headers.common["Authorization"] = undefined;
-                return navigate("/login", { replace: true });
-              }}
+              className={`flex items-center gap-4 px-4 py-3 text-lg font-medium text-unFocusedText duration-300`}
             >
               <div> {link.Icon} </div>
               <p>{link.name}</p>
@@ -80,6 +100,6 @@ const Sidebar = () => {
       </section>
     </div>
   );
-}
+};
 
-export default Sidebar
+export default Sidebar;
--- src/components/global/TopNav.tsx
+++ src/components/global/TopNav.tsx
@@ -1,48 +1,73 @@
-import { useLocation } from "react-router-dom"
-import { H1 } from "./Typography"
-import { useEffect, useState } from "react"
-import { navLinks } from "@/utils/data"
+import { H1 } from "./Typography";
+import { useState } from "react";
+import { navLinks } from "@/utils/data";
+import useChangeRoute from "@/hooks/custom/useChangeRoute";
 
 const TopNav = () => {
-	const location = useLocation()
-	const [pageName, setPageName] = useState('')
+  const [pageName, setPageName] = useState("");
 
-	useEffect(() => {
-		// check current page and set page name
-		const page = navLinks.find(navLink => location.pathname.includes(navLink.link))
-		setPageName(page?.name || '')
-	}, [location.pathname])
+  const user = JSON.parse(localStorage.getItem("fmw_business_user") || "{}");
 
-	return (
-		<div className='h-20 border-b pr-11 pl-8 py-3.5 flex justify-between items-center bg-white border-sidebarBorder'>
+  useChangeRoute((pathname) => {
+    // check current page and set page name
+    const page = navLinks.find((navLink) => pathname.includes(navLink.link));
+    setPageName(page?.name || "");
+  });
 
-			<div>
-				<H1>{pageName} </H1>
-			</div>
+  return (
+    <div className="flex h-20 items-center justify-between border-b border-sidebarBorder bg-white py-3.5 pl-8 pr-11">
+      <div>
+        <H1>{pageName} </H1>
+      </div>
 
+      <div className="flex items-center gap-7">
+        <p className="rounded-xl bg-white px-5 py-3 text-primaryBlue shadow-username">
+          {user?.name || "User"}
+        </p>
+        <div className="flex items-center gap-2.5">
+          <div className="flex h-11 w-11 items-center justify-center rounded-full bg-[#E7F0FF]">
+            <svg
+              width="28"
+              height="28"
+              viewBox="0 0 28 28"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g filter="url(#filter0_d_1995_7617)">
+                <circle cx="76" cy="38.5" r="22" fill="#E7F0FF" />
+              </g>
+              <path
+                d="M14.0011 2.3335C10.9445 2.3335 8.45947 4.8185 8.45947 7.87516C8.45947 10.8735 10.8045 13.3002 13.8611 13.4052C13.9545 13.3935 14.0478 13.3935 14.1178 13.4052C14.1411 13.4052 14.1528 13.4052 14.1761 13.4052C14.1878 13.4052 14.1878 13.4052 14.1995 13.4052C17.1861 13.3002 19.5311 10.8735 19.5428 7.87516C19.5428 4.8185 17.0578 2.3335 14.0011 2.3335Z"
+                fill="#A8CAFF"
+              />
+              <path
+                d="M19.9276 16.5079C16.6726 14.3379 11.3643 14.3379 8.08593 16.5079C6.60426 17.4995 5.7876 18.8412 5.7876 20.2762C5.7876 21.7112 6.60426 23.0412 8.07426 24.0212C9.7076 25.1179 11.8543 25.6662 14.0009 25.6662C16.1476 25.6662 18.2943 25.1179 19.9276 24.0212C21.3976 23.0295 22.2143 21.6995 22.2143 20.2529C22.2026 18.8179 21.3976 17.4879 19.9276 16.5079Z"
+                fill="#A8CAFF"
+              />
+            </svg>
+          </div>
 
-			<div className="flex items-center gap-7">
-				<p className="">
-					Owner
-				</p>
-				<div className="flex items-center gap-2.5">
-					<div className="w-11 h-11 rounded-full flex justify-center items-center bg-[#E7F0FF]">
-						<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<g filter="url(#filter0_d_1995_7617)">
-								<circle cx="76" cy="38.5" r="22" fill="#E7F0FF" />
-							</g>
-							<path d="M14.0011 2.3335C10.9445 2.3335 8.45947 4.8185 8.45947 7.87516C8.45947 10.8735 10.8045 13.3002 13.8611 13.4052C13.9545 13.3935 14.0478 13.3935 14.1178 13.4052C14.1411 13.4052 14.1528 13.4052 14.1761 13.4052C14.1878 13.4052 14.1878 13.4052 14.1995 13.4052C17.1861 13.3002 19.5311 10.8735 19.5428 7.87516C19.5428 4.8185 17.0578 2.3335 14.0011 2.3335Z" fill="#A8CAFF" />
-							<path d="M19.9276 16.5079C16.6726 14.3379 11.3643 14.3379 8.08593 16.5079C6.60426 17.4995 5.7876 18.8412 5.7876 20.2762C5.7876 21.7112 6.60426 23.0412 8.07426 24.0212C9.7076 25.1179 11.8543 25.6662 14.0009 25.6662C16.1476 25.6662 18.2943 25.1179 19.9276 24.0212C21.3976 23.0295 22.2143 21.6995 22.2143 20.2529C22.2026 18.8179 21.3976 17.4879 19.9276 16.5079Z" fill="#A8CAFF" />
-						</svg>
-					</div>
+          <svg
+            className="cursor-pointer"
+            width="18"
+            height="18"
+            viewBox="0 0 24 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M19.9181 9.4502L13.3981 15.9702C12.6281 16.7402 11.3681 16.7402 10.5981 15.9702L4.07812 9.4502"
+              stroke="#18191F"
+              strokeWidth="1.5"
+              strokeMiterlimit="10"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-					<svg className="cursor-pointer" width="18" height="18" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d="M19.9181 9.4502L13.3981 15.9702C12.6281 16.7402 11.3681 16.7402 10.5981 15.9702L4.07812 9.4502" stroke="#18191F" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
-					</svg>
-				</div>
-			</div>
-		</div>
-	)
-}
-
-export default TopNav
+export default TopNav;
