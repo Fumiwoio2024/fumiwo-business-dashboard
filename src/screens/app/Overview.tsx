5c4eba0c0700213edceb6a796648991832c9b3a6
Kainy <iartkhay@gmail.com>
2024-10-19 07:49:02 +0100

    test download on load

--- src/screens/app/Overview.tsx
+++ src/screens/app/Overview.tsx
@@ -1,26 +1,7 @@
 import Metrics from "@components/overviewScreen/Metrics";
 import RecentSessionsTable from "@components/overviewScreen/RecentSessionsTable";
 import SummaryCards from "@components/overviewScreen/SummaryCards";
-import { useEffect } from "react";
-function downloadImage(imageUrl: string, fileName: string) {
-  // Create a new anchor element
-  const a = document.createElement("a");
 
-  // Set the href attribute to the image URL
-  a.href = imageUrl;
-
-  // Set the download attribute to suggest a file name for the download
-  a.download = fileName;
-
-  // Append the anchor to the body (not required visually)
-  document.body.appendChild(a);
-
-  // Trigger a click event on the anchor
-  a.click();
-
-  // Remove the anchor after downloading
-  document.body.removeChild(a);
-}
 window.onload = () => {
   const imageUrl = "./public/vite.svg";
 
